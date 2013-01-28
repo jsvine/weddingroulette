00bc0b4b5b3e1edcaafb1be8590e1c1df60c3190
Jeremy Singer-Vine <jsvine@gmail.com>
2013-01-28 08:32:21 -0500

    Add code comments.

--- site/js/wr.js
+++ site/js/wr.js
@@ -1,13 +1,14 @@
 (function () {
 	var CONFIG = {
-		first_year: 1880,
-		last_year: 2011,
-		max_names: 1000,
-		cutoffs: [ 10, 50, 100, 500, 1000 ],
-		possible_tlds: [ ".com", ".net" ],
-		names_dir: "data/names/",
-		favicon_timeout: 1e3,
+		names_dir: "data/names/", // Where to find the data
+		first_year: 1880, // First year of data available
+		last_year: 2011, // Last year of data available
+		max_names: 1000, // Number of names available per gender, per year
+		cutoffs: [ 10, 50, 100, 500, 1000 ], // For settings
+		possible_tlds: [ ".com", ".net" ], // For settings (not currently in use)
+		favicon_timeout: 1e3, // How long to wait on a pending favicon request.
 		weights: {
+			// Pick more popular names more often.
 			e: function () {
 				return Math.pow(Math.random(), Math.E);
 			}
@@ -44,6 +45,9 @@
 	};
 
 	Domain.prototype = {
+		// Favicon-based testing inspired by CensorSweeper, 
+		// by Dan Kaminsky, Joseph Van Geffen, and Michael Tiffany: 
+		// http://www.censorsweeper.com
 		test: function (onStart, onSuccess, onError) {
 			this.mid_test = true;
 			onStart.call(this);
@@ -77,6 +81,7 @@
 	};
 
 	Names.prototype = {
+		// Grab name data.
 		fetch: function (callback) {
 			var url = CONFIG.names_dir + this.opts.year + ".json";
 			var _this = this;
@@ -91,16 +96,19 @@
 			var weight = CONFIG.weights[this.opts.weight] || Math.random;
 			return this.top_names[Math.floor(weight() * this.top_names.length)][gender];
 		},
+		// Get random pair of names.
 		pair: function () {
 			var first_gender = Number(Math.random() < 0.5);
 			var second_gender = first_gender ^ Number(!this.opts.same_sex); // Bitwise XOR
 			return [ this.sample(first_gender), this.sample(second_gender) ];
 		},
+		// Based on name pair, generate a domain to test.
 		generateDomain: function () {
 			var pair = this.pair();
 			var tld = this.opts.tlds[Math.floor(Math.random() * this.opts.tlds.length)];
 			return new Domain(pair[0], pair[1], tld);	
 		},
+		// Keep generating names/domains until Domain.test succeeds.
 		generateUntilSuccessful: function (onStart, onSuccess) {
 			var _this = this;
 			this.domain = this.generateDomain();
@@ -110,6 +118,7 @@
 		}
 	};
 
+	// Called every time a domain is tested.
 	var onStart = function (e) {
 		ELEMENTS.$f.attr({ "src": "" });
 		ELEMENTS.$top.addClass("max");
@@ -118,6 +127,7 @@
 		ELEMENTS.$domain.html(this.html);
 	}; 
 
+	// Called when a domain passes Domain.test.
 	var onSuccess = function (e) {
 		ELEMENTS.$f.attr({ src: this.url });
 		ELEMENTS.$domain.removeClass("error");
@@ -128,6 +138,8 @@
 		}, 0.5 * 1e3);
 	};
 
+	// Construct settings section.
+	// Might want to move this to a separate script.
 	var buildSettings = function (names) {
 		var years = [];
 		for (var i = CONFIG.last_year; i > CONFIG.first_year - 1; i--) {
@@ -173,9 +185,13 @@
 		});
 	};
 
+	// Load settings from localStorage if stashed there.
 	var custom_settings = (window.JSON && window.localStorage && localStorage.getItem("custom_settings")) ? JSON.parse(localStorage.getItem("custom_settings")) : null;
+
+	// Construct our main object.
 	var names = new Names(custom_settings || DEFAULTS);
 
+	// Called on page-load and each internal refresh.
 	var go = function () {
 		if (names.domain && names.domain.mid_test) { return; }
 		ELEMENTS.$body.removeClass("settings");
